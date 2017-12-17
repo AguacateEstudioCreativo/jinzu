a051f2629b81dc7c22266c1d4b50465acbc82cad
dannegm <im@danielgarcia.me>
2017-12-17 02:18:28 -0600

    Brand

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Jinzu</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -83,8 +83,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://aguacatecreativo.com/">Aguacate Estudio Creativo</a>
+                    <a href="https://github.com/AguacateEstudioCreativo/jinzu">GitHub</a>
                 </div>
             </div>
         </div>
